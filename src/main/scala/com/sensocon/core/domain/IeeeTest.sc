dfab806bc14b2eafe57b6664d3a30cbff169238f
Jerry Napoli <ilopanj@me.com>
2018-08-31 17:33:53 -0400

    Flattened temperature and pressure from mongo query

--- src/main/scala/com/sensocon/core/domain/IeeeTest.sc
+++ src/main/scala/com/sensocon/core/domain/IeeeTest.sc
@@ -1,5 +1,17 @@
 package com.sensocon.core.domain
-import sca
+import scala.collection.JavaConverters._
+
 object IeeeTest {
-  println("Welcome to the Scala worksheet")
+  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
+  
+  import MongoLoraPacket._
+  
+  
+  val tempbuf = Array( 198, 237, 124, 66 ).toList.asJava
+                                                  //> tempbuf  : java.util.List[Int] = [198, 237, 124, 66]
+   
+  val floattemp = toFloat(tempbuf)                //> floattemp  : Float = 63.2322
+  
+
+  
 }
--- src/main/scala/com/sensocon/core/domain/MongoLoraPacket.scala
+++ src/main/scala/com/sensocon/core/domain/MongoLoraPacket.scala
@@ -2,15 +2,17 @@ package com.sensocon.core.domain
 import scalaz._
 import Scalaz._
 import scala.beans.BeanProperty
+import scala.collection.JavaConverters._
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.annotation.Id
-import com.f
 import java.time.Instant
+import com.fasterxml.jackson.annotation.JsonGetter
 
 
 @Document
 class MongoLoraPacket
 {
+  import MongoLoraPacket._
     @Id
   @BeanProperty
   var messageId: String = ""
@@ -24,19 +26,22 @@ class MongoLoraPacket
   var frequency: Double = 0.0
   @BeanProperty
   var gatewayId: String = ""
-  //@BeanProperty
+  @BeanProperty
   var pressure: java.util.List[Int] = new java.util.ArrayList[Int] 
   @BeanProperty
   var raw: java.util.List[Double] = new java.util.ArrayList[Double] 
   @BeanProperty
   var rssi: Double = 0.0
-  //@BeanProperty
+  @BeanProperty
   var temperature: java.util.List[Int] = new java.util.ArrayList[Int] 
   @BeanProperty
   var timestamp: Instant  = Instant.now 
   
   @JsonGetter
-  def temperatureFarenheit = temperature.toList
+  def temperatureFarenheit = toFloat(temperature)
+  
+  @JsonGetter
+  def pressurePsi = toFloat(pressure)
 }
 
 object MongoLoraPacket
@@ -45,15 +50,12 @@ object MongoLoraPacket
   /** extract an integer at position bp from buf
    */
   def getInt(buf: List[Int]): Int =
-    ((buf(0  ) & 0xff) << 24) +
-    ((buf(1) & 0xff) << 16) +
-    ((buf(2) & 0xff) << 8) +
-     (buf(3) & 0xff)
+    ((buf(3  ) & 0xff) << 24) +
+    ((buf(2) & 0xff) << 16) +
+    ((buf(1) & 0xff) << 8) +
+     (buf(0) & 0xff)
      
-  def toFloat(buf: java.util.List[Int]) = {
-   
-    val intval = getInt(buf.toList)
-    
-  }
+  def toFloat(buf: java.util.List[Int]) = 
+    java.lang.Float.intBitsToFloat(getInt(buf.asScala.toList))
   
 }
